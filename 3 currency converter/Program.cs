a591784600b2099fde820491e1fa5254a9dd0c94
Virvon <dan130205@gmail.com>
2022-10-01 11:29:18 +0300

    currency converter

--- 3 currency converter/Program.cs
+++ 3 currency converter/Program.cs
@@ -44,7 +44,7 @@ namespace _3_currency_converter
                 Console.WriteLine("Ваш баланс\n\nВалюта X: " + currencyX + "\nВалюта Y: " + currencyY + "\nВалюта Z: " + currencyZ);
                 Console.WriteLine("\nКурс конвертера\n\n" + currencyXrate + " валюта X = " + currencyYrate + " валюта Y = " + currencyZrate + " валюта Z");
                 Console.SetCursorPosition(0, 0);
-                Console.Write("Конвертер валют\n\n1.Обмен валюты X на Y\n2.Обмен валюты X на Z\n3.Обмен валюты Y на X\n4.Обмен валюты Y на Z\n5.Обмен валюты Z на X\n6.Обмен валюты Z на Y\n7.Выход из системы\n\nВведите номер команды: ");
+                Console.Write("Конвертер валют\n\n" + CommandXToY + ".Обмен валюты X на Y\n" + CommandXToZ + ".Обмен валюты X на Z\n" + CommandYToX + ".Обмен валюты Y на X\n" + CommandYToZ + ".Обмен валюты Y на Z\n" + CommandZToX + ".Обмен валюты Z на X\n" + CommandZToY + ".Обмен валюты Z на Y\n" + CommandExit +".Выход из системы\n\nВведите номер команды: ");
                 switch (Console.ReadLine())
                 {
                     case CommandXToY:
